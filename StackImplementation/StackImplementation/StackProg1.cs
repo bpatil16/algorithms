d0d74e589e40d33493ad350cad57b779b67746c9
sachip <sachip@microsoft.com>
2016-07-17 23:07:08 -0700

    Commiting stack Prog2

--- StackImplementation/StackImplementation/StackProg1.cs
+++ StackImplementation/StackImplementation/StackProg1.cs
@@ -8,44 +8,44 @@ namespace StackImplementation
 {
     class StackProg1
     {
-        public static void Main()
-        {
-            string S = "2*(3+(5(sas))";
-            int len = S.Length;
-            stack st = new stack(len - 1);
-            int temp = 0;
+        //public static void Main()
+        //{
+        //    string S = "2*(3+(5(sas))";
+        //    int len = S.Length;
+        //    stack st = new stack(len - 1);
+        //    int temp = 0;
 
-            foreach (char c in S)
-            {
-                if (c == '(')
-                {
-                    st.push(c);
-                    temp = temp+1;
+        //    foreach (char c in S)
+        //    {
+        //        if (c == '(')
+        //        {
+        //            st.push(c);
+        //            temp = temp+1;
 
-                }
-                else if (c == ')')
-                {
-                    char element = (char)st.Pop();
+        //        }
+        //        else if (c == ')')
+        //        {
+        //            char element = (char)st.Pop();
                     
-                    if (element == '(')
-                    {
-                        Console.WriteLine("\n Found matching parenthesis for {0} (",temp);
-                        temp--;
-                    }
-                    else if (st.IsEmpty() == false)
-                    {
-                        Console.WriteLine("\n Found a missing paranthesis");
-                    }
+        //            if (element == '(')
+        //            {
+        //                Console.WriteLine("\n Found matching parenthesis for {0} (",temp);
+        //                temp--;
+        //            }
+        //            else if (st.IsEmpty() == false)
+        //            {
+        //                Console.WriteLine("\n Found a missing paranthesis");
+        //            }
 
-                }
-            }
+        //        }
+        //    }
 
-               if (st.IsEmpty() == false)
-                {
-                    Console.WriteLine("\n Found a missing paranthesis for {0} parenthesis", temp);
-                }
+        //       if (st.IsEmpty() == false)
+        //        {
+        //            Console.WriteLine("\n Found a missing paranthesis for {0} parenthesis", temp);
+        //        }
             
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
     }
 }
