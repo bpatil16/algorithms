48889b8f3c1fb5c086bd5695cea08f33841b8fab
sachip <sachip@microsoft.com>
2016-07-24 21:12:44 -0700

    commiting Subarrays Program

---
+++ Answer1/Answer1/Subarray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Answer1
+{
+    public class Subarray
+    {
+
+        public class IndexInArray
+        {
+            public int start = 0;
+            public int end = 0;
+        }
+
+        public void Test2(int[] input)
+        {
+
+            //define output list to store index for printing result
+
+            var output = new List<IndexInArray>();
+
+
+            // declare dictionary  to store value and index of elements sum     
+
+            var ht = new Dictionary<int, List<int>>();
+
+            // Maintains sum of elements so far
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += input[i];
+                if (sum == 0)
+                {
+                    output.Add(new IndexInArray { start = 0, end = i });
+                }
+                if (ht.ContainsKey(sum))
+                {
+                    List<int> vc = ht[sum];
+                    foreach (var id in vc)
+                    {
+                        output.Add(new IndexInArray { start = (id + 1), end = i });
+                    }
+                    vc.Add(i);
+                }
+                else
+                {
+                    ht.Add(sum, new List<int>() { i });
+                }
+            }
+
+            //print output
+            foreach (var o in output)
+            {
+                Console.WriteLine("Found subarray with start={0}, end={1}", o.start, o.end);
+            }
+        }
+
+        public static void Main()
+        {
+            int[] input = new int[] { 6, 3, -1, -3, 4, -2, 2, 4, 6, -12, -7 };
+
+            Subarray Obj7 = new Subarray();
+            Obj7.Test2(input);
+            Console.ReadLine();
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ StringsAlgo/StringsAlgo/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Arr = new char[5];
+            Arr[0] = 'H';
+            Arr[1] = 'E';
+            Arr[2] = 'l';
+            Arr[3] = 'l';
+            Arr[4] = 'o';
+            int len = (Arr.Length)-1;
+            Program obj1 = new Program();
+            obj1.quicksort(Arr, 0, len);
+            bool result = checkUnique(Arr);
+            if (result == true)
+            {
+                Console.WriteLine("all character in string are unique");
+            }
+            else
+            { Console.WriteLine("there is a duplicate"); }
+
+            Console.ReadLine();
+        }
+
+        static bool checkUnique(char []Arr)
+    {
+        for(int i = 0; i < (Arr.Length) - 1; ++i)
+            if(Arr[i] == Arr[i + 1])
+                return false;
+           
+          return true;
+    }
+ 
+
+        public void quicksort(char[] Arr, int l, int r)
+        {
+
+            int index = divide(Arr, l, r);
+            if(l<index-1)
+                quicksort(Arr, l, index-1);
+            if(r>index)
+                quicksort(Arr, index, r);
+        }   
+      
+
+        public int divide(char[] arr, int left, int right)
+        {
+            char pivot = arr[(left + right) / 2];
+            while(left<=right)
+            {
+                while (arr[left] < pivot) left++;
+                while (arr[right]> pivot) right--;
+
+                if (left <= right){
+                    interchange(arr, left, right);
+                     left++;
+                    right--;}
+            }
+            return left;
+        }
+        
+
+        public void interchange(char[] arr, int a, int b)
+        {
+            char temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+    }
+
+
+}
+
